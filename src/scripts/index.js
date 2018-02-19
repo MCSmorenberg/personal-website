cc926b7539f49e68caaa5d1548e6ecd47c652fc3
MCSmorenberg <mcsmorenberg@gmail.com>
2018-02-19 17:20:05 +0100

    Add more webpack config like babel

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,9 @@
 (function () {
-  console.log('JS goes here!');
+    function sayHiES5(greeting){
+        return greeting;
+    }
+    console.log(sayHiES5('Hello ES5!'));
+
+    const sayHiES6 = greeting => greeting
+    console.log(sayHiES6('Hello ES6!'));
 })();
